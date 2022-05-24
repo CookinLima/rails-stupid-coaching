54b499da8035575fae5222b628ca4527f9fc8fe0
Kenny Wong <kken.w@hotmail.com>
2022-05-24 14:06:25 +0800

    update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,12 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+require("@popperjs/core");
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import "channels";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap";
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import "channels"
-
-Rails.start()
-Turbolinks.start()
+Rails.start();
+Turbolinks.start();
